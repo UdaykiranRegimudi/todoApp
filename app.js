c31badfe4cc45d4d2a92c30445ac2c10d35a76ce
Udaykiran <uuday3804@gmail.com>
2022-01-31 16:21:31 +0000

    todo app

---
+++ app.js
@@ -0,0 +1,110 @@
+/*
+ *  Created a Table with name todo in the todoApplication.db file using the CLI.
+ */
+
+const express = require("express");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+
+const databasePath = path.join(__dirname, "todoApplication.db");
+
+const app = express();
+
+app.use(express.json());
+
+let database = null;
+
+const initializeDbAndServer = async () => {
+  try {
+    database = await open({
+      filename: databasePath,
+      driver: sqlite3.Database,
+    });
+
+    app.listen(process.env.PORT || 3001, () =>
+      console.log("Server Running at http://localhost:3001/")
+    );
+  } catch (error) {
+    console.log(`DB Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDbAndServer();
+
+app.get("/todos/", async (request, response) => {
+  let data = null;
+  const getTodosQuery = `select * from todo`;
+  data = await database.all(getTodosQuery);
+  response.send(data);
+});
+
+app.get("/todos/:todoId/", async (request, response) => {
+  const { todoId } = request.params;
+
+  const getTodoQuery = `
+    SELECT
+      *
+    FROM
+      todo
+    WHERE
+      id = ${todoId};`;
+  const todo = await database.get(getTodoQuery);
+  response.send(todo);
+});
+
+app.post("/todos/", async (request, response) => {
+  const { id, todo, isChecked } = request.body;
+  const postTodoQuery = `
+  INSERT INTO
+    todo (id, todo, is_checked)
+  VALUES
+    (${id}, '${todo}', '${isChecked}');`;
+  await database.run(postTodoQuery);
+  response.send("Todo Successfully Added");
+});
+
+app.put("/todos/:todoId/", async (request, response) => {
+  const { todoId } = request.params;
+  let updateColumn = "";
+  const previousTodoQuery = `
+    SELECT
+      *
+    FROM
+      todo
+    WHERE 
+      id = ${todoId};`;
+  const previousTodo = await database.get(previousTodoQuery);
+
+  const {
+    todo = previousTodo.todo,
+    isChecked = previousTodo.isChecked,
+  } = request.body;
+
+  const updateTodoQuery = `
+    UPDATE
+      todo
+    SET
+      todo='${todo}',
+      is_checked='${isChecked}'
+    WHERE
+      id = ${todoId};`;
+
+  await database.run(updateTodoQuery);
+  response.send(`Updated`);
+});
+
+app.delete("/todos/:todoId/", async (request, response) => {
+  const { todoId } = request.params;
+  const deleteTodoQuery = `
+  DELETE FROM
+    todo
+  WHERE
+    id = ${todoId};`;
+
+  await database.run(deleteTodoQuery);
+  response.send("Todo Deleted");
+});
+
+module.exports = app;
